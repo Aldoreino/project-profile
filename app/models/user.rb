f782d4f649663c1ff3c934798100ddbf8199d912
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-27 16:36:24 -0700

    additional tests created for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :profiles
 	validates :name, presence: true
+	validates :password, presence: true #throws errors
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,28 @@ RSpec.describe User, :type => :model do
 		# will it raise an error without a name?
 		expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
 	end
+	it "has an email" do
+		# does the email exist?
+		expect(subject).to respond_to(:email)
+	end
+	it "is invalid without an email" do
+		# can a user be created without an email?
+		expect(subject).to be_invalid
+	end
+	it "raises an error without an email" do
+		# will it raise an error without an email?
+		expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
+	end
+	it "has a password" do
+		# does the password exist?
+		expect(subject).to respond_to(:password_digest)
+	end
+	it "is invalid without a password" do
+		# can a user be created without a password?
+		expect(subject).to be_invalid
+	end
+	it "raises an error without a password" do
+		# will it raise an error without a password?
+		expect{subject.save!}.to raise_error(ActiveRecord::RecordInvalid)
+	end
 end
