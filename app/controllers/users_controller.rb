84b9a3b3f32d509fc05c876519ebd58868a11f2e
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-06-01 12:33:01 -0700

    sign up form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new
+		if @user.save
+			redirect_to '/'
+		else 
+			render 'new'
+		end
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_many :profiles
 	validates :name, presence: true
 	validates :password, presence: true #throws errors
+	has_secure_password
 end
